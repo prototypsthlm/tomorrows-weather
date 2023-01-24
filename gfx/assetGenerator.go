34d8b01f854dfcba7f06d81ea94a858c5b21451f
driatic <fredrik.hasselgren@prototyp.se>
2023-01-24 16:39:41 +0100

    TOM-29: rand select cloud image

--- gfx/assetGenerator.go
+++ gfx/assetGenerator.go
@@ -4,8 +4,10 @@ import (
 	"github.com/faiface/pixel"
 	"golang.org/x/image/colornames"
 	"image/color"
+	"math/rand"
 	"prototyp.com/tomorrows-weather/models"
 	"prototyp.com/tomorrows-weather/utils"
+	"strconv"
 )
 
 func generateBackground(dailyForecast models.DailyForecast) color.RGBA {
@@ -18,20 +20,22 @@ func generateBackground(dailyForecast models.DailyForecast) color.RGBA {
 func generateClouds(dailyForecast models.DailyForecast) (sprites []*pixel.Sprite, animationSpeed float64) {
 	var cloudSprites []*pixel.Sprite
 	animationSpeed = 0
-	println("dailyForecast.Clouds" + string(dailyForecast.Clouds))
-	clouds := dailyForecast.Clouds / 10
+	cloudDensity := dailyForecast.Clouds / 10
 
-	//cloud density
-	for i := 0; i < clouds; i++ {
-		//todo: rand select image
-		//todo: rand select position
-		pic1, err := loadPicture("./assets/svg/cloud1.png")
+	//todo: 10 cloud assets is probably overkill, shade background color instead?
+	for i := 0; i < cloudDensity; i++ {
+		srcImage := rand.Intn(10-1) + 1
+		pic1, err := loadPicture("./assets/png/clouds/" + strconv.Itoa(srcImage) + ".png")
 		if err != nil {
 			panic(err)
 		}
 
+		//todo: rand select position via bounds?
 		cloudSprites = append(cloudSprites, pixel.NewSprite(pic1, pic1.Bounds()))
 	}
+
+	//todo: calc animationSpeed based on wind
+	//todo: should be slight random to create layers
 
 	return cloudSprites, animationSpeed
 }
