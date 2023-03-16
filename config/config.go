6fe755f6635c7142111cbe1ef1c2bdcc1e5c047f
0x4d <0x4d@undefined>
2023-03-16 21:31:07 +0100

    Add new

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ import "time"
 
 const WindowWidth = 468
 const WindowHeight = 468
-const OpenWeatherMapApiKey = "ea42e6f5e54a1766d53c3ab30605f7ea"
+const OpenWeatherMapApiKey = ""
 const MaxClouds = 32
 const MaxRaindrops = 512
 const HTTPTimeout = time.Minute * 5
