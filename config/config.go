2cb72ce55a6f8f97ec49267a031ac50d91612b4d
0x4d <0x4d@undefined>
2023-03-17 17:00:52 +0100

    Update check forecast interval

--- config/config.go
+++ config/config.go
@@ -9,5 +9,5 @@ const MaxClouds = 32
 const MaxRaindrops = 512
 const HTTPTimeout = time.Minute * 5
 const WindSpeedModifier = 0.1
-const UpdateWeatherInterval = -5 * time.Second
+const UpdateWeatherInterval = -15 * time.Minute
 const DefaultWeatherId = 800
